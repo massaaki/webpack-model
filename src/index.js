dad864a03070ff52dfc2068bae8939d8df8032df
Massaaki <mauricio.massaaki@gmail.com>
2021-03-10 22:11:29 -0300

    feat: Add corejs and regenerator to run moderm resources in old browsers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+//Enable modern resources on old browsers
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+
 import './assets/css/style.css';
 console.log('just a simple template with webpack configs');
